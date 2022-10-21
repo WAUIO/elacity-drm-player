f615a3b90359318e26209a6efc95b321cefda094
Irzhy Ranaivoarivony <irzhy.ran@gmail.com>
2022-10-21 13:18:20 +0300

    attempt to customize css

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -3,12 +3,26 @@ import type { FC, PropsWithChildren } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, AppSettingsProvider } from '@elacity-js/uikit';
 import { JsonLocalStorage } from '@elacity-js/lib';
+import type { ThemeOptions } from '@mui/material/styles';
 import { SnackbarProvider } from 'notistack';
 import { Provider as ReduxProvider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import Web3Provider from './lib/web3/Provider';
 import { ConnectorProvider } from './lib/web3/ConnectorContext';
 import { store } from './state/store';
+
+// @todo: make this works: check @elacity-js/uikit
+const themeCustomization = {
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '& .Footer-root': {},
+        },
+      },
+    },
+  },
+} as ThemeOptions;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface AppProviderProps {}
@@ -41,7 +55,12 @@ const AppProvider: FC<PropsWithChildren<AppProviderProps>> = ({
             ],
           }}
         >
-          <ThemeProvider customization={{}}>
+          <ThemeProvider
+            customization={{
+              light: themeCustomization,
+              dark: themeCustomization,
+            }}
+          >
             <SnackbarProvider anchorOrigin={{ horizontal: 'center', vertical: 'top' }} autoHideDuration={5000}>
               <ConnectorProvider redirectTo="/create">
                 <BrowserRouter>
